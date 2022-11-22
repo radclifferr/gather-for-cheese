1c1e8a19133d5d2eeeac2a6ecf3befdb342235d3
Richard Radcliffe <radcliffer94@gmail.com>
2022-11-22 13:43:26 -0800

    Add home images and create home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
 import React from "react";
 import "../styles/Home.css"
+import heroImageOne from "../images/hero-image-one.jpg"
+import heroImageTwo from "../images/hero-image-two.jpg"
+import exploreCuttingBoards from "../images/explore-cutting-boards.jpg"
+import exploreCharcuterieBoards from "../images/explore-charcuterie-boards.jpg"
 
 const Home = () => {
     return (
-        <div className="content-container">Home Page</div>
+        <div className="home-page-container">
+            <img src={heroImageOne} alt="" className="left-hero-image"/>
+            <div className="hero-copy">
+                <div className="welcome">Welcome to Gather for Cheese</div>
+                <div className="welcome-copy">Custom cutting boards and charcuterie boards for that special someone in your life</div>
+                <img src={heroImageTwo} alt="" className="right-hero-image" />
+            </div>
+
+            <img src={exploreCuttingBoards} alt="" className="explore-images left" />
+            <img src={exploreCharcuterieBoards} alt="" className="explore-images right" />
+
+        </div>
 
     )
 }
