376366609c1fce388296d558a0c4fff93b3bdf5a
Richard Radcliffe <radcliffer94@gmail.com>
2022-11-22 12:55:44 -0800

    Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,19 +5,22 @@ import logoRectangle from "../images/logo-rectangle.png"
 const Footer = () => {
     return (
         <div className="footer">
-            <img src={logoRectangle} alt="" className="footer-logo"/>
-            <div>
-                <div className="footer-tab">Home</div>
-                <div className="footer-tab">Shop</div>
-                <div className="footer-tab">Blog</div>
-                <div className="footer-tab">About</div>
-                <div className="footer-tab">Contact</div>
-                <div className="footer-tab">Cart</div>
+            <div className="footer-logo-container">
+                <img src={logoRectangle} alt="" className="footer-logo"/>
+                <div>Custom cutting board and charcuterie boards for that special someone in your life. </div>
             </div>
-            <div>© 2022 GatherForCheese.com</div>
-            <div>
+            <ul className="footer-quicklink-container">
+                <li className="quick-link-header">Quick Links</li>
+                <li className="footer-tab">Shop</li>
+                <li className="footer-tab">Blog</li>
+                <li className="footer-tab">About</li>
+                <li className="footer-tab">Contact</li>
+            </ul>
+            
+            <div className="policy-link-container">
                 <div>Returns And Refunds Policy</div>
                 <div>Terms of Service And Privacy Policy</div>
+                <div>© 2022 GatherForCheese.com</div>
             </div>
         </div>
 
