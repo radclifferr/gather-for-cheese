f8a800fab4f3a3bdac14f1a98fc0ce7941658bae
Richard Radcliffe <radcliffer94@gmail.com>
2022-11-23 11:49:52 -0800

    Add policy pages

---
+++ src/components/ReturnsRefunds.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ReturnsRefunds = () => {
+    return (
+        <div>Returns and Refunds Page</div>
+    )
+}
+export default ReturnsRefunds;
---
+++ src/components/TermsOfService.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const TermsOfService = () => {
+    return (
+        <div>RTerms Of Service Page</div>
+    )
+}
+export default TermsOfService;
