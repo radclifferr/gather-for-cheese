8e50d6e8505b1a43cc9bbfb1f26b5d14036b9fd3
Richard Radcliffe <radcliffer94@gmail.com>
2022-11-23 12:36:53 -0800

    Link footer logo and style links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,28 +7,33 @@ const Footer = () => {
     return (
         <div className="footer">
             <div className="footer-logo-container">
-                <img src={logoRectangle} alt="" className="footer-logo"/>
+                <Link to= "/" ><img src={logoRectangle} className="logo-container" alt="" /></Link>
                 <div>Custom cutting board and charcuterie boards for that special someone in your life. </div>
             </div>
             <ul className="footer-quicklink-container">
                 <li className="quick-link-header">Quick Links</li>
-                <li>
-                    <Link to="/Shop" className="footer-tab">Shop</Link>
+                <li className="footer-tab">
+                    <Link to="/Shop" >Shop</Link>
                 </li>
-                <li>
-                    <Link to="/Blog" className="footer-tab">Blog</Link>
+                <li className="footer-tab">
+                    <Link to="/Blog" >Blog</Link>
                 </li>
-                <li>
-                    <Link to="/Contact" className="footer-tab">Contact</Link>
+                <li className="footer-tab">
+                    <Link to="/Contact" >Contact</Link>
                 </li>
             </ul>
             
-            <div className="policy-link-container">
-                <Link to="/Returns-Refunds-Policy">Returns And Refunds Policy</Link>
-                <Link to="/Terms-Of-Service">Terms Of Service And Privacy Policy</Link>
-                <div>© 2022 GatherForCheese.com</div>
-                
-            </div>
+            <ul className="policy-links-container">
+                <li>
+                    <Link to="/Returns-Refunds-Policy">Returns And Refunds Policy</Link>
+                </li>
+                <li>
+                    <Link to="/Terms-Of-Service">Terms Of Service And Privacy Policy</Link>
+                </li>
+                <li>
+                    <div>© 2022 GatherForCheese.com</div>
+                </li>
+            </ul>
         </div>
 
     )
