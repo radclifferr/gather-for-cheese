0e0370c09b09364b11879ed9f61c938ca82485ae
Richard Radcliffe <radcliffer94@gmail.com>
2022-11-26 11:06:37 -0800

    Create and style blog page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import "../styles/Blog.css"
+import BlogPostOne from "../images/blog-post-one.jpg"
 
 const Blog = () => {
+    const [getBlogPosts, setBlogPosts] = useState([
+        {
+            postTitle: "The Best Baked Brie",
+            postHeaderImage: BlogPostOne,
+            postShortDescription: "The best brie recipe for parties",
+            postLongDescription: "The best brie recipe for parties, you know its true", 
+        },
+        {
+            postTitle: "The Best Baked Brie",
+            postHeaderImage: BlogPostOne,
+            postShortDescription: "The best brie recipe for parties",
+            postLongDescription: "The best brie recipe for parties, you know its true", 
+        },
+        {
+            postTitle: "The Best Baked Brie",
+            postHeaderImage: BlogPostOne,
+            postShortDescription: "The best brie recipe for parties",
+            postLongDescription: "The best brie recipe for parties, you know its true", 
+        },
+    ])
     return (
-        "Blog Page"
+        <div className="blog-page-container">
+            <h1>Blog Posts</h1>
+            <div className="blog-post-container">
+                {getBlogPosts.map((post, index) => {
+                    return (
+                        <div className="post-container">
+                            <div className="post-title">{post.postTitle}</div>
+                            <img src={post.postHeaderImage} alt="" />
+                            <div className="post-short-description">{post.postShortDescription}</div>
+                        </div>
+
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
