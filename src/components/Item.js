c6f355a61e9eb6da597767e2c03ea07c0ceb0d6f
radclifferr <radcliffer94@gmail.com>
2023-01-01 18:36:07 -0800

    Fix add to cart personalization  bug with shallow copy of item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation, Link} from "react-router-dom"
+import { useLocation} from "react-router-dom"
 import "../styles/Item.css"
 
 const Item = (props) => {
@@ -7,12 +7,15 @@ const Item = (props) => {
     const index = location.state.itemNumber
     let product = props.getItems[index]
 
-    const onAddPersonalization = (e) => {
-        product.itemPersonalization= e.target.value
+
+    const handleAddToCart = (e) => {
+        e.preventDefault()
+        let productCopy = {...product}
+        let personalValue = e.nativeEvent.path[1][0].value
+        props.addItemToCart(productCopy, personalValue)
     }
 
     return (
-        console.log(product.itemPersonalization),
         <div className="item-page-container">
             <img src={product.itemMainImage} alt=""  className="product-image"/>
             <div>
@@ -21,12 +24,10 @@ const Item = (props) => {
                 <div>${product.itemPrice}</div>
                 <form>
                     <label htmlFor ="personalization">Enter your product personalization: </label>
-                    <input type="text" id="personalization" maxLength={20} required onChange={onAddPersonalization}/>
+                    <input type="text" id="personalization" maxLength={20} required />
+                    <button onClick = {handleAddToCart}>Add To Cart</button>
                 </form>
-                <button onClick = {() => {
-                    props.addItemToCart(product)
-                    
-                     }}>Add To Cart</button>
+                
             </div>
             
 
