748ab39c1a40a2ca91a4b672b27134d62032fd40
Richard Radcliffe <radcliffer94@gmail.com>
2023-01-05 16:47:26 -0800

    Structure shop items page with JSX description

--- src/components/shopItems.js
+++ src/components/shopItems.js
@@ -1,36 +1,122 @@
-const heroImageOne = require("../images/hero-image-one.jpg")
+
+
 
 const shopItems = [
     {
-        itemName: "Cutting Board 1",
-        itemDescription: "This cutting board is great", 
-        itemMainImage: heroImageOne,
-        itemPrice: 200, 
-        itemNumber: 0,
+        itemName: "The Home For The Holidays Cutting Board",
+        itemDescription:         
+            <>
+                <h2>Make Their House a Home</h2> 
+                <p>A gift they will cherish and use for a lifetime. Let them know that the love they share is special and precious. The memories they will share in the kitchen together surrounded by family and friends will bring warmth and love into their hearts. Your gift will always be the centerpiece of their home.</p>
+                <p>Remind this family every day when they step in their kitchen of the commitment and care they have for one another.  Crafted from butter-smooth butcher block, this personalized heirloom cutting board expresses their love for one another in every detail.</p>
+            </>,
+        images: [
+            require("../images/products/ebf/1.jpg"),
+            require("../images/products/ebf/2.jpg"),
+            require("../images/products/ebf/3.jpg"),
+            require("../images/products/ebf/4.jpg"),
+            require("../images/products/ebf/5.jpg"),
+            require("../images/products/ebf/6.jpg"),
+            require("../images/products/ebf/7.jpg"),
+            require("../images/products/ebf/8.jpg"),
+            require("../images/products/ebf/9.jpg"),
+            require("../images/products/ebf/10.jpg"),
+        ],
+        itemPrice: 335, 
         itemPersonalization: "",
     },
     {
-        itemName: "Cutting Board 1",
-        itemDescription: "This cutting board is great", 
-        itemMainImage: heroImageOne,
-        itemPrice: 350, 
-        itemNumber: 1,
+        itemName: "The Home Together Charcuterie Board",
+        itemDescription: "Forget your plans and cozy up for a night at home on the sofa. Just the right size to serve you and that special someone for a romantic, cozy, and special night spent at home. The moments you two spend together are as precious as gold. Embrace the significance of these memories you two create over a warm cup of tea and a sweet treat. Dark exotic wood contrasts your family monogram proudly and serves as a spot to slice your favorite smoky cheese.", 
+        images: [
+            require("../images/products/b/1.jpg"),
+            require("../images/products/b/2.jpg"),
+            require("../images/products/b/3.jpg"),
+            require("../images/products/b/4.jpg"),
+            require("../images/products/b/5.jpg"),
+            require("../images/products/b/6.jpg"),
+        ],
+        itemPrice: 89, 
         itemPersonalization: ""
     },
     {
-        itemName: "Cutting Board 1",
-        itemDescription: "This cutting board is great", 
-        itemMainImage: heroImageOne,
-        itemPrice: 152,
-        itemNumber: 2,
+        itemName: "The Precious Memories Cutting Board",
+        itemDescription: 
+        <>
+            <h2>Make Their House a Home.</h2>
+            <p> See their face light up as they unwrap a gift that expresses how much they mean to you.</p>
+            <p>They will remember the warm cozy nights spent making memories in the kitchen together, with you always in their heart. Preparing meals will remind them of how you created a home that always provided the love and nurturing they needed.</p>
+            <p> This personalized butcher block cutting board, showcasing the family monogram, will always be the centerpiece of their home.</p>
+        </>, 
+        images: [
+            require("../images/products/ferguson/1.jpg"),
+            require("../images/products/ferguson/2.jpg"),
+            require("../images/products/ferguson/3.jpg"),
+            require("../images/products/ferguson/4.jpg"),
+            require("../images/products/ferguson/5.jpg"),
+            require("../images/products/ferguson/6.jpg"),
+            require("../images/products/ferguson/7.jpg"),
+        ],
+        itemPrice: 189,
         itemPersonalization: ""
     },
     {
-        itemName: "Cutting Board 1",
-        itemDescription: "This cutting board is great", 
-        itemMainImage: heroImageOne, 
-        itemPrice: 175,
-        itemNumber: 3,
+        itemName: "The Home Forever Cutting Board",
+        itemDescription: 
+        <>
+            <h2>Welcome Them Home With Love After A Lifetime Of Dedication</h2>
+            <p>Show that special someone in your life the hard work and dedication they put into their military career was valued. Welcome them home with a butcher block cutting board that shows pride in military service in every detail. </p>
+            <p>Embody their military unit in deep contrasting hardwood to remind them of the commitment they made to themselves, their unit, and to their country.</p>
+            <p>MILITARY UNIT PERSONALIZATION INSTRUCTIONS.</p>
+            <p>Please enter the unit name and designation you would like on your new cutting board. We will reach out to you within 24 hours with renderings of a proposed design to ensure that the unit crest and guideon design are correct. Reach out to us to add a witty personalized message or special engraving on the back of the cutting board to remind them just how much the military meant to them and to you! Any design you can think of can be engraved all for only an additional $25. We would love to work with you through the design you had in mind for that special someone's homecoming.</p>
+        </>,
+        images: [
+            require("../images/products/military/1.jpg"),
+            require("../images/products/military/2.jpg"),
+            require("../images/products/military/3.jpg"),
+            require("../images/products/military/4.jpg"),
+            require("../images/products/military/5.jpg"),
+        ],
+        itemPrice: 323,
+        itemPersonalization: ""
+    },
+    {
+        itemName: "The Special Memories Cutting Board",
+        itemDescription:
+        <>
+            <h2>Make Their House a Home.</h2>
+            <p>A gift they will cherish and use for a lifetime. Let them know that the love they share is special and precious. The memories they will share in the kitchen together surrounded by family and friends will bring warmth and love into their hearts. </p>
+            <p> Your gift will always be the centerpiece of their home. Remind this family every day when they step in their kitchen of the commitment and care they have for one another. Crafted from butter-smooth butcher block, this personalized heirloom cutting board expresses their love for one another in every detail.</p>
+        </>,
+        images: [
+            require("../images/products/church/1.jpg"),
+            require("../images/products/church/2.jpg"),
+            require("../images/products/church/3.jpg"),
+            require("../images/products/church/4.jpg"),
+            require("../images/products/church/5.jpg"),
+            require("../images/products/church/6.jpg"),
+            require("../images/products/church/7.jpg"),
+        ],
+        itemPrice: 319,
+        itemPersonalization: ""
+    },
+    {
+        itemName: "The Little Moments Charcuterie Board",
+        itemDescription: 
+        <>
+            <h2>Set the stage for a heartwarming party when everyone is home</h2>
+            <p>Your carefully selected wine and cheese pairings will shine with this functional and decorative charcuterie board.</p>
+            <p>With space for everything and gorgeous satin nickel finishings, your new personalized charcuterie board will make every moment spent with family that much more special.</p>
+        </>,
+        images: [
+            require("../images/products/smith/1.jpg"),
+            require("../images/products/smith/2.jpg"),
+            require("../images/products/smith/3.jpg"),
+            require("../images/products/smith/4.jpg"),
+            require("../images/products/smith/5.jpg"),
+            require("../images/products/smith/6.jpg"),
+        ],
+        itemPrice: 169,
         itemPersonalization: ""
     },
   ]
