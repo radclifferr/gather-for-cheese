9378f67d7f82579719230b3f6b92921d1e4bc79a
Richard Radcliffe <radcliffer94@gmail.com>
2023-01-07 13:45:04 -0800

    Pass key prop to map function

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -9,9 +9,8 @@ const Shop = (props) => {
             <div className="shop-container">
                 {props.getItems.map((item, index) => {
                     return (
-                    <div className="item-card-container" key= {item.itemNumber} >
+                    <div className="item-card-container" key= {index} >
                         <Link to = {`/Shop/${item.itemName}` } state={{index}}><img src={item.images[0]} className="item-main-image" alt="" /></Link>
-                        
                         <div className="item-card-name">{item.itemName}</div>
                         <div className="item-card-price">${item.itemPrice}</div>
                         <Link to = {`/Shop/${item.itemName}` } state={{index}}><button className="general-button">See Item Details</button></Link>
