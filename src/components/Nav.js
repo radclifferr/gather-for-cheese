e1dbecef6aef4da111887582058e74f82b58ea98
Richard Radcliffe <radcliffer94@gmail.com>
2023-01-07 16:50:54 -0800

    Style nav bar for small screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Nav.css"
 import ShoppingCart from "../images/shopping-cart.svg"
 import logoRectangle from "../images/logo-rectangle.png"
@@ -6,21 +6,54 @@ import { Link, NavLink } from "react-router-dom"
 
 
 const Nav = (props) => {
+    const [windowDimension, setWindowDimension] = useState(null);
+
+    useEffect(() => {
+      setWindowDimension(window.innerWidth);
+    }, []);
+  
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimension(window.innerWidth);
+      }
+  
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+  
+    const isMobile = windowDimension <= 850;
+
+
+
+
+
     return (
-        <div className="nav-bar-container">
-            <Link to= "/" ><img className="header-logo-container" src={logoRectangle} alt="" /></Link>
-            <div className="tab-container">
-                <NavLink to= "/" className="tab">Home</NavLink>
-                <NavLink to= "/Shop"className="tab">Shop</NavLink>
-                <NavLink to= "/Blog" className="tab">Blog</NavLink>
-                <NavLink to= "/Contact" className="tab">Contact</NavLink>
+        isMobile ? (
+            <div className="nav-bar-container">
+                <Link to= "/" ><img className="header-logo-container" src={logoRectangle} alt="" /></Link>
+                <div className="tab-container">
+                    <NavLink to= "/" className="tab">Home</NavLink>
+                    <NavLink to= "/Shop"className="tab">Shop</NavLink>
+                    <NavLink to= "/Blog" className="tab">Blog</NavLink>
+                    <NavLink to= "/Contact" className="tab">Contact</NavLink>
+                    <NavLink to= "/Cart" className = "tab">Cart</NavLink>
+                </div>
             </div>
-            <Link to= "/Cart" className = "shopping-cart">
-                <img  src={ShoppingCart} alt=""/>
-                <div className="cart-quantity">{props.getCart.length}</div>
-            </Link>
-        </div>
-        
+        ) : (
+            <div className="nav-bar-container">
+                <Link to= "/" ><img className="header-logo-container" src={logoRectangle} alt="" /></Link>
+                <div className="tab-container">
+                    <NavLink to= "/" className="tab">Home</NavLink>
+                    <NavLink to= "/Shop"className="tab">Shop</NavLink>
+                    <NavLink to= "/Blog" className="tab">Blog</NavLink>
+                    <NavLink to= "/Contact" className="tab">Contact</NavLink>
+                </div>
+                <Link to= "/Cart" className = "shopping-cart">
+                    <img  src={ShoppingCart} alt=""/>
+                    <div className="cart-quantity">{props.getCart.length}</div>
+                </Link>
+            </div>
+        )
     )
 }
 
